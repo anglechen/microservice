5baf9c483fc49acfb81199d3671a70f79412652a
aubrey <676719570@qq.com>
2020-01-09 15:04:36 +0800

    添加 自动打包成镜像

--- helloword/src/main/java/com/test/helloword/UserController.java
+++ helloword/src/main/java/com/test/helloword/UserController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
 	
 //	@RequestMapping("/users")
-//	@RequestMapping(value = "/users",method =  )
+	@RequestMapping(value = "/users",method = RequestMethod.GET )
 	@GetMapping
 	public String list() {
 		return "用户列表";
@@ -28,7 +29,7 @@ public class UserController {
 //	POST /users
 	@PostMapping
 	public String add(User user) {
-		return "新增成功：" + user.getName();
+		return "新增成功：" + user.getUsername();
 		
 	}
 	
